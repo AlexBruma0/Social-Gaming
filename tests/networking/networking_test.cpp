507141607ed31c80ea18b6671969d4e33dd468a2
Evan Mangat <evanmangat@gmail.com>
2023-10-17 14:53:56 -0700

    Update networking_test.cpp

--- tests/networking/networking_test.cpp
+++ tests/networking/networking_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "Client.h"
+#include "Server.h" 
 
 // Demonstrate some basic assertions.
 TEST(NetworkingTests, Initial) {
@@ -8,4 +9,116 @@ TEST(NetworkingTests, Initial) {
 
   // Testing you can access the client from the networking library
   networking::Client client{"localhost", "8000"};
+}
+
+TEST(ClientTest, ConstructorInitializesWithoutErrors) {
+    networking::Client client("localhost", "8000");
+    // If the constructor fails, it might throw an exception, so reaching this line would mean success.
+    SUCCEED();
+}
+
+TEST(ClientTest, SendAndReceiveMessages) {
+    networking::Client client("localhost", "8000");
+    client.send("TestMessage");
+    client.update();  // Simulate an update cycle.
+    EXPECT_EQ(client.receive(), "TestMessage");
+}
+
+TEST(ClientTest, OnMessageReceivesMessages) {
+    networking::Client client(...);
+    client.onMessage("TestMessage"); // Simulate receiving a message.
+    // Check if incoming buffer contains the message.
+    // This assumes there's a method or way to check the incoming buffer.
+    EXPECT_EQ(client.getIncomingMessage(), "TestMessage");
+}
+
+TEST(ClientTest, CheckDisconnectionState) {
+    networking::Client client("localhost", "8000");
+    // Assuming some method or scenario causes disconnection here.
+    client.update();  // Simulate an update cycle.
+    EXPECT_TRUE(client.isDisconnected());
+}
+
+
+TEST(ClientTest, OnErrorDisconnects) {
+    networking::Client client("localhost", "8000");
+    client.onError(); // Simulate error event.
+    // Check if client is disconnected.
+    EXPECT_FALSE(client.isConnected());
+}
+
+TEST(ClientTest, UpdateHandlesExceptions) {
+    networking::Client client("wrong_address", "8000");
+    // If 'update' throws exceptions when it encounters issues, this test will check that.
+    EXPECT_THROW(client.update(), std::exception); 
+}
+
+TEST(ClientTest, SendAndReceiveMultipleMessages) {
+    networking::Client client("localhost", "8000");
+    client.send("TestMessage1");
+    client.send("TestMessage2");
+    client.update();  // Assume this sends and then receives messages.
+
+    // If 'receive' returns a concatenated string of all received messages:
+    std::string expected_message = "TestMessage1TestMessage2";
+    EXPECT_EQ(client.receive(), expected_message);
+}
+
+
+TEST(ServerTest, ServerReceivesMessagesCorrectly) {
+    networking::Server server(8000, "SomeHTTPMessage");  // Instantiate server with necessary arguments.
+    server.update();  // Simulate an update cycle.
+    auto messages = server.receive();
+    // Assuming some mechanism to check if a specific message is in the deque.
+    EXPECT_TRUE(contains(messages, "TestMessage"));  // `contains` is a hypothetical helper function.
+}
+
+TEST(ServerTest, ServerSendsMessagesCorrectly) {
+    networking::Server server(8000, "SomeHTTPMessage");
+    std::deque<Message> messagesToSend = { /* ... populate with some messages ... */ };
+    server.send(messagesToSend);
+    server.update();  // Simulate an update cycle.
+    // Assuming some method or mechanism to verify that the messages were sent.
+    EXPECT_TRUE(server.messagesSentSuccessfully());
+}
+
+
+TEST(ServerTest, HandlesNewConnectionsCorrectly) {
+    networking::Server server(8000, "SomeHTTPMessage");
+    server.update();  // Simulate an update cycle.
+    // Assuming some method or mechanism to check if a new client connected.
+    EXPECT_TRUE(server.newClientConnected());
+}
+
+
+TEST(ServerTest, HandlesClientDisconnectionsCorrectly) {
+    networking::Server server(8000, "SomeHTTPMessage");
+    server.update();  // Simulate an update cycle.
+    // Assuming some method or mechanism to check if a client disconnected.
+    EXPECT_TRUE(server.clientDisconnected());
+}
+
+TEST(ServerTest, UpdateHandlesExceptions) {
+    networking::Server server(0, "InvalidHTTPMessage");  // Using '0' might cause issues if it's an invalid port.
+    // If 'update' throws exceptions when it encounters issues, this test will check that.
+    EXPECT_THROW(server.update(), std::exception); 
+}
+
+TEST(ServerTest, HandleMultipleConnections) {
+    networking::Server server(8000, "SomeHTTPMessage");
+    // Simulate multiple client connections here. You might need to mock this or check through received messages.
+    server.update();  // Handle any new connections.
+    // Check if multiple clients are connected. The method/function used here is hypothetical.
+    EXPECT_GE(server.getNumberOfClients(), 2);  // Check if at least two clients are connected.
+}
+
+TEST(ServerTest, BroadcastsToAllClients) {
+    networking::Server server(8000, "SomeHTTPMessage");
+    std::deque<Message> messagesToSend = { /* ... populate with some messages ... */ };
+    server.send(messagesToSend);  // Assume this broadcasts the message to all clients.
+    server.update();
+
+    // Here, you would need a way to verify that all connected clients received the message.
+    // This might involve mocking the clients or having a method on the server that confirms delivery.
+    EXPECT_TRUE(server.allClientsReceivedMessages());
 }
