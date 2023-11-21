2c4d0028fa20395a74bd1581afbea5faca21457e
lrohwede <lex_rohweder@sfu.ca>
2023-11-20 16:40:39 -0800

    more tests

--- tests/networking/messageQueue_test.cpp
+++ tests/networking/messageQueue_test.cpp
@@ -28,3 +28,18 @@ TEST(messageQueue_tests, idTest){
     ASSERT_EQ(mq.getMessageFromID(con1)[0].text, input);
     ASSERT_EQ(mq.getMessageFromID(con2)[0].text, input2);
 }
+
+TEST(messageQueue_tests, idNotFoundTest){
+    MessageQueue mq = MessageQueue();
+    std::string input = "input";
+    std::string input2 = "input2";
+    networking::Connection con1{8888};
+    networking::Message msg1{con1, input};
+    networking::Connection con2{9999};
+    networking::Message msg2{con2, input2};
+    networking::Connection falseConnection{7777};
+    mq.add(msg1);
+    mq.add(msg2);
+    ASSERT_TRUE(mq.getMessageFromID(falseConnection).empty());
+    ASSERT_FALSE(mq.getMessageFromID(con2).empty());
+}
