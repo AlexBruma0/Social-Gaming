81345adf569ffa75167f5175b6d61392f4670ccf
kfc7 <kfc7@sfu.ca>
2023-11-26 15:48:05 -0800

    moved GameServer class into the networking library rather than the executable, so we can unit test and invoke methods elsewhere

---
+++ lib/networking/include/gameServer.h
@@ -0,0 +1,16 @@
+//
+// Created by kevin on 11/26/2023.
+//
+
+#ifndef KLAH_GAMESERVER_H
+#define KLAH_GAMESERVER_H
+
+
+
+class gameServer {
+
+};
+
+
+
+#endif //KLAH_GAMESERVER_H
---
+++ lib/networking/src/gameServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kevin on 11/26/2023.
+//
+
+#include "gameServer.h"
